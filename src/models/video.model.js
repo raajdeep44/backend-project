9003d5f5ed6d87fca83a900a134ace339cee5d07
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-07-16 12:44:45 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video",videoSchema)
